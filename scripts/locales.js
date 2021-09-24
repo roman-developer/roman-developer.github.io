77ebc587c0f9f0e7d63f1d4598f387c3b70dd197
roman-developer <jose.maria.roman87@gmail.com>
2021-09-24 15:03:02 +0200

    Medium link

--- scripts/locales.js
+++ scripts/locales.js
@@ -3,12 +3,14 @@ var language = {
     welcome: "Hola!",
     intro: "Soy Jose, Ingeniero Software",
     maintenance1: "Esta página está en mantenimiento",
-    maintenance2: "Disculpa las molestias"
+    maintenance2: "Disculpa las molestias",
+    medium_link: "Visita mi perfil en Medium"
   },
   en: {
     welcome: "Hi!",
     intro: "I'm Jose, Software Engineer",
     maintenance1: "This page is under maintenance",
-    maintenance2: "Sorry for the inconvenience"
+    maintenance2: "Sorry for the inconvenience",
+    medium_link: "Visit my Medium profile"
   }        
 };
